27e6d37a15e09b4ca5bd2ea1ba8f30111891d748
Fodor Ciprian <fodorciprian13@gmail.com>
2018-05-12 01:02:52 +0300

    [FrontEnd]Added Dctor Registration Directive

---
+++ src/main/resources/static/scripts/directives/DoctorRegisterDirective.js
@@ -0,0 +1,33 @@
+(function () {
+    'use strict'
+    var app = angular.module('RedDrop');
+    app.directive('registerDoctor', function () {
+        return {
+            scope: {},
+            templateUrl: "views/directives/DoctorRegistrationDirective.html",
+            controller: function () {
+
+                var vm = this;
+
+                vm.tryRegisterDoctor = function () {
+                    http({
+                        method: 'POST',
+                        url: apiIP + '/api/doctor/add',
+                        data: {
+                            uuid: "",
+                            password: vm.doctorPassword,
+                            name : vm.nameRegister,
+                            cnp : vm.doctorCNP
+                        }
+                    }).then(function (response) {
+                        if (response.data.succesful) {
+                            this.close();
+                        }
+                    }, function (error) {
+                    });
+                }
+            },
+            controllerAs: "ctrl"
+        };
+    });
+})();
