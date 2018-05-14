d039237d01846065fc090ad4eef665f02c4d9720
Istrate Andrei <andreiistrate14@yahoo.com>
2018-05-14 23:22:16 +0300

    [FrontEnd]Implemented donator register page structure

--- src/main/resources/static/scripts/controllers/LandingController.js
+++ src/main/resources/static/scripts/controllers/LandingController.js
@@ -7,11 +7,14 @@
 
         vm.goToLoginDonator = function () {
             $location.path("/donator/login");
-        }
+        };
 
         vm.goToLoginDoctor = function () {
             $location.path("/doctor/login");
-        }
+        };
 
+        vm.goToLoginAdmin = function () {
+            $location.path("/admin/login");
+        };
     }]);
 })();
