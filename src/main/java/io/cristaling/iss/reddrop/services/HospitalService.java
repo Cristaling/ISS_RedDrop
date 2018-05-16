59625098a869817ebcfd81211fe9bfc78d70d9b3
Rares Iova <iovarares@gmail.com>
2018-05-16 20:09:58 +0300

    [BackEnd]Integrated PermissionService into hospital RSC

--- src/main/java/io/cristaling/iss/reddrop/services/HospitalService.java
+++ src/main/java/io/cristaling/iss/reddrop/services/HospitalService.java
@@ -1,16 +1,35 @@
 package io.cristaling.iss.reddrop.services;
 
+import io.cristaling.iss.reddrop.core.Hospital;
 import io.cristaling.iss.reddrop.repositories.HospitalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.UUID;
+
 @Service
 public class HospitalService {
 
-    HospitalRepository repository;
+    HospitalRepository hospitalRepository;
 
     @Autowired
-    public HospitalService(HospitalRepository repository) {
-        this.repository = repository;
+    public HospitalService(HospitalRepository hospitalRepository) {
+        this.hospitalRepository = hospitalRepository;
     }
+
+    public void deleteHospital(UUID uuid) {
+        hospitalRepository.deleteById(uuid);
+    }
+
+    public void registerHospital(Hospital hospital) {
+        //TODO Validate Doctor
+        hospital.setUuid(UUID.randomUUID());
+        hospitalRepository.save(hospital);
+    }
+
+    public List<Hospital> getAllHospitals() {
+        return hospitalRepository.findAll();
+    }
+
 }
--- src/main/java/io/cristaling/iss/reddrop/web/controllers/HospitalController.java
+++ src/main/java/io/cristaling/iss/reddrop/web/controllers/HospitalController.java
@@ -3,6 +3,8 @@ package io.cristaling.iss.reddrop.web.controllers;
 import io.cristaling.iss.reddrop.core.Hospital;
 import io.cristaling.iss.reddrop.repositories.HospitalRepository;
 import io.cristaling.iss.reddrop.services.HospitalService;
+import io.cristaling.iss.reddrop.services.PermissionsService;
+import io.cristaling.iss.reddrop.utils.Permission;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,29 +18,39 @@ import java.util.UUID;
 @RequestMapping("/api/hospital")
 public class HospitalController {
 
-	HospitalRepository hospitalRepository;
+	HospitalService hospitalService;
+	PermissionsService permissionsService;
 
 	@Autowired
-	public HospitalController(HospitalRepository hospitalRepository) {
-		this.hospitalRepository=hospitalRepository;
+	public HospitalController(HospitalService hospitalService, PermissionsService permissionsService) {
+		this.hospitalService = hospitalService;
+		this.permissionsService = permissionsService;
 	}
 
 	@RequestMapping("/getall")
-	public List<Hospital> getAllHospitals(){
-		return hospitalRepository.findAll();
+	public List<Hospital> getAllHospitals(UUID token) {
+		if (!permissionsService.hasPermission(token, Permission.ADMIN)) {
+			return null;
+		}
+		return hospitalService.getAllHospitals();
 	}
 
 	@RequestMapping("/add")
 	@ResponseStatus(value = HttpStatus.NO_CONTENT)
-	public void addHospital(@RequestBody Hospital hospital){
-		hospital.setUuid(UUID.randomUUID());
-		hospitalRepository.save(hospital);
+	public void addHospital(UUID token, @RequestBody Hospital hospital) {
+		if (!permissionsService.hasPermission(token, Permission.ADMIN)) {
+			return;
+		}
+		hospitalService.registerHospital(hospital);
 	}
 
 	@RequestMapping("/delete")
 	@ResponseStatus(value = HttpStatus.NO_CONTENT)
-	public void deleteHospital(String uuid){
-		hospitalRepository.deleteById(UUID.fromString(uuid));
+	public void deleteHospital(UUID token, UUID uuid) {
+		if (!permissionsService.hasPermission(token, Permission.ADMIN)) {
+			return;
+		}
+		hospitalService.deleteHospital(uuid);
 	}
 
 }
