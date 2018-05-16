1f60ff6a2af8ed664d5b225aa6546dcf94ff3dce
Fodor Ciprian <fodorciprian13@gmail.com>
2018-05-16 20:19:59 +0300

    [BackEnd]Modified BloodBag Controller

--- src/main/java/io/cristaling/iss/reddrop/services/BloodRequestService.java
+++ src/main/java/io/cristaling/iss/reddrop/services/BloodRequestService.java
@@ -1,16 +1,40 @@
 package io.cristaling.iss.reddrop.services;
 
+import io.cristaling.iss.reddrop.core.BloodRequest;
+import io.cristaling.iss.reddrop.core.Doctor;
 import io.cristaling.iss.reddrop.repositories.BloodRequestRepository;
+import io.cristaling.iss.reddrop.repositories.DoctorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class BloodRequestService {
 
-    BloodRequestRepository repository;
-
+    BloodRequestRepository requestRepository;
+    DoctorRepository doctorRepository;
     @Autowired
-    public BloodRequestService(BloodRequestRepository repository) {
-        this.repository = repository;
+    public BloodRequestService(BloodRequestRepository repository,DoctorRepository doctorRepository) {
+        this.requestRepository = repository;
+        this.doctorRepository=doctorRepository;
+    }
+
+    public void save(BloodRequest bloodRequest){
+        requestRepository.save(bloodRequest);
+    }
+
+    public void deleteById(String uuid){
+        requestRepository.deleteById(UUID.fromString(uuid));
+    }
+
+    public List<BloodRequest> findAll(){
+        return requestRepository.findAll();
+    }
+
+    public List<BloodRequest> findSpecific(String uuid){
+        Doctor doctor=doctorRepository.getOne(UUID.fromString(uuid));
+        return requestRepository.getBloodRequestsByDoctorEquals(doctor);
     }
 }
--- src/main/java/io/cristaling/iss/reddrop/web/controllers/BloodRequestController.java
+++ src/main/java/io/cristaling/iss/reddrop/web/controllers/BloodRequestController.java
@@ -5,6 +5,7 @@ import io.cristaling.iss.reddrop.core.BloodBag;
 import io.cristaling.iss.reddrop.core.BloodRequest;
 import io.cristaling.iss.reddrop.core.Doctor;
 import io.cristaling.iss.reddrop.repositories.BloodRequestRepository;
+import io.cristaling.iss.reddrop.services.BloodRequestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -17,35 +18,33 @@ import java.util.UUID;
 @RequestMapping("/api/bloodrequest")
 public class BloodRequestController {
 
-    BloodRequestRepository bloodRequestRepository;
+    BloodRequestService bloodRequestService;
 
     @Autowired
-    public BloodRequestController(BloodRequestRepository bloodRequestRepository) {
-        this.bloodRequestRepository = bloodRequestRepository;
+    public BloodRequestController(BloodRequestService bloodRequestService) {
+        this.bloodRequestService = bloodRequestService;
     }
 
     @RequestMapping("/add")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void addRequest(@RequestBody BloodRequest bloodRequest){
-        bloodRequestRepository.save(bloodRequest);
+        bloodRequestService.save(bloodRequest);
     }
 
     @RequestMapping("/delete")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void deleteRequest(String uuid){
-        bloodRequestRepository.deleteById(UUID.fromString(uuid));
+        bloodRequestService.deleteById(uuid);
     }
 
 
     @RequestMapping("/getall")
     public List<BloodRequest> getAllRequests(){
-        return bloodRequestRepository.findAll();
+        return bloodRequestService.findAll();
     }
 
     @RequestMapping("/getfrom")
     public List<BloodRequest> getSpecificRequests(String uuid){
-        Doctor doctor=new Doctor();
-        doctor.setUuid(UUID.fromString(uuid));
-        return bloodRequestRepository.getBloodRequestsByDoctorEquals(doctor);
+        return bloodRequestService.findSpecific(uuid);
     }
 }
