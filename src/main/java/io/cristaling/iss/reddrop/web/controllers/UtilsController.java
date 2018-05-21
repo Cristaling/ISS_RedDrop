e5eaa31b9aafd6d2d4d11458969df58854906588
Fodor Ciprian <fodorciprian13@gmail.com>
2018-05-21 18:23:04 +0300

    [BackEnd]Added Utils Controller

---
+++ src/main/java/io/cristaling/iss/reddrop/web/controllers/UtilsController.java
@@ -0,0 +1,21 @@
+package io.cristaling.iss.reddrop.web.controllers;
+
+
+import io.cristaling.iss.reddrop.utils.Diseases;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/utils")
+public class UtilsController {
+
+
+    @RequestMapping("/getdiseases")
+    public Diseases[] getDiseases(){
+        return Diseases.values();
+    }
+}
