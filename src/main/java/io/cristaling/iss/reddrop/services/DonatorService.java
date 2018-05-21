a20c11e80ceb13f32c2f59ccc0ea15f58e3918e6
Rares Iova <iovarares@gmail.com>
2018-05-21 18:12:14 +0300

    [BackEnd]Added more data to mock donator

--- src/main/java/io/cristaling/iss/reddrop/services/DonatorService.java
+++ src/main/java/io/cristaling/iss/reddrop/services/DonatorService.java
@@ -19,6 +19,9 @@ public class DonatorService {
         donator.setUuid(UUID.randomUUID());
         donator.setCnp("1971211055084");
         donator.setPassword("1971211055084");
+        donator.setNume("Iova");
+        donator.setPrenume("Rares");
+        donator.setNrTel("0751080998");
         donatorRepository.save(donator);
     }
 
