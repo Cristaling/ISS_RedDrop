8a1d51bc5fc580d7a7e663258dcfc762d2f0e822
Rares Iova <iovarares@gmail.com>
2018-05-21 18:07:48 +0300

    [BackEnd]Added security to donation visit fetching

--- src/main/java/io/cristaling/iss/reddrop/web/controllers/DonationVisitController.java
+++ src/main/java/io/cristaling/iss/reddrop/web/controllers/DonationVisitController.java
@@ -7,36 +7,42 @@ import io.cristaling.iss.reddrop.services.PermissionsService;
 import io.cristaling.iss.reddrop.utils.Permission;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.UUID;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/api/donationvisit")
 public class DonationVisitController {
 
-    DonationVisitService donationVisitService;
-    PermissionsService permissionsService;
+	DonationVisitService donationVisitService;
+	PermissionsService permissionsService;
 
-    @Autowired
-    public DonationVisitController(PermissionsService permissionsService,DonationVisitService donationVisitService) {
-        this.permissionsService=permissionsService;
-        this.donationVisitService = donationVisitService;
-    }
+	@Autowired
+	public DonationVisitController(PermissionsService permissionsService, DonationVisitService donationVisitService) {
+		this.permissionsService = permissionsService;
+		this.donationVisitService = donationVisitService;
+	}
 
-    @RequestMapping("/add")
-    @ResponseStatus(value = HttpStatus.NO_CONTENT)
-    public void addVisit(String token,@RequestBody DonationVisit donationVisit){
-        if (!permissionsService.hasPermission(token, Permission.DONATOR)) {
-            return;
-        }
-        donationVisitService.addDonationVisit(donationVisit);
-    }
+	@RequestMapping("/add")
+	@ResponseStatus(value = HttpStatus.NO_CONTENT)
+	public void addVisit(String token, @RequestBody DonationVisit donationVisit) {
+		if (!permissionsService.hasPermission(token, Permission.DONATOR)) {
+			return;
+		}
+		donationVisitService.addDonationVisit(donationVisit);
+	}
 
-    @RequestMapping("/getall")
-    public List<DonationVisit> getAllDonationVisits(){
-        return donationVisitService.findAll();
-    }
+	@RequestMapping("/getall")
+	public List<DonationVisit> getAllDonationVisits(String token) {
+		if (!permissionsService.hasPermission(token, Permission.ADMIN)) {
+			return null;
+		}
+		return donationVisitService.findAll();
+	}
 }
